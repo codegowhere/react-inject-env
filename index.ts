f1bc8693d4cc1a67eac4ae5191d3d74a825d3c98
lynn <7008316+es-lynn@users.noreply.github.com>
2021-10-11 11:37:53 +0800

    react-inject-env
    # Conflicts:
    #	package.json

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
-import { replaceFilesInDir } from './src/Something'
-;(async function() {
-  try {
-    const dir = process.argv[2]
-    if (!dir) {
-      throw Error('No directory entered. Usage npx react-inject-env <path_to_build_folder>')
-    }
-    replaceFilesInDir(dir)
-  } catch (e) {
-    console.error((e?.name ?? 'Error') + ': ' + e?.message)
-    process.exit(1)
-  }
-})()
+import { InjectEnvCommandLine } from './src/InjectEnv'
+
+const commandLine = new InjectEnvCommandLine()
+commandLine.execute()
