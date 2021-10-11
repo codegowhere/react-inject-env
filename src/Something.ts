09690380291325da055ad5636d83ddfdc011f1d1
lynn <7008316+es-lynn@users.noreply.github.com>
2021-10-11 11:37:30 +0800

    working prototype

--- src/Something.ts
+++ src/Something.ts
@@ -1,24 +1,53 @@
-class some_singleton {
-  field = '1'
-  test(): string {
-    console.log('some_singleton')
-    return this.field
+import replace from 'replace-in-file'
+
+function generateFromTo(
+  envCfg: Record<string, string>,
+  prefix = ['REACT_APP_', 'REPLACE_']
+): {
+  from: string[] | RegExp[];
+  to: string[];
+} {
+  const from = Object.keys(envCfg)
+    .map(key => key.replace(prefix[0], prefix[1]))
+    .map(key => new RegExp(`\\b${key}\\b`, 'g'))
+  const to = Object.values(envCfg)
+  return {
+    from: from,
+    to: to
   }
 }
-export default new some_singleton()
 
-export class some_class {
-  field: string
-  constructor(field: string) {
-    this.field = field
-  }
-  test(): string {
-    console.log('some_class')
-    return this.field
-  }
+function replaceFile(dirPath: string, envConfig: Record<string, string>) {
+  const { from, to } = generateFromTo(envConfig)
+  const results = replace.sync({
+    files: `${dirPath}/**/*`,
+    from: from,
+    to: to,
+    countMatches: true
+  })
+  results.forEach(it => {
+    if (it.hasChanged) {
+      console.info(`Replaced ${it.numReplacements} variable(s) in '${it.file}'`)
+    }
+  })
 }
 
-export function some_function(field: string): string {
-  console.log('some_function')
-  return field
+function getReactEnvCfg(): Record<string, string> {
+  const env = process.env
+  const keys = Object.keys(env)
+  const reactKeys = keys.filter(key => key.startsWith('REACT_APP_'))
+
+  const envCfg: Record<string, string> = {}
+  for (const key of reactKeys) {
+    // @ts-ignore
+    envCfg[key] = process.env[key]
+  }
+  return envCfg
+}
+
+export function replaceFilesInDir(dir: string) {
+  const envCfg = getReactEnvCfg()
+  console.info('Injecting the following environment variables:')
+  console.info(envCfg)
+  replaceFile(dir, envCfg)
 }
